449ac5e81556563c549850127679106b6550e0c2
gabrielbaute <gabrielbaute@gmail.com>
2026-07-26 11:34:13 -0400

    Implementamos un router con las rutas actuales para navegar

---
+++ lib/ui/router/app_router.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../views/promedio_view.dart';
+import '../views/realtime_view.dart';
+
+final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>();
+
+/// Router centralizado de la aplicación.
+class AppRouter {
+  AppRouter._();
+
+  static final GoRouter router = GoRouter(
+    navigatorKey: _rootNavigatorKey,
+    initialLocation: '/promedio',
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/promedio',
+        builder: (BuildContext context, GoRouterState state) {
+          return PromedioView(currentPath: state.uri.toString());
+        },
+      ),
+      GoRoute(
+        path: '/realtime',
+        builder: (BuildContext context, GoRouterState state) {
+          return RealtimeView(currentPath: state.uri.toString());
+        },
+      ),
+      GoRoute(
+        path: '/history',
+        builder: (BuildContext context, GoRouterState state) {
+          // Placeholder temporal para el historial
+          return const Center(child: Text('Vista Historial'));
+        },
+      ),
+    ],
+  );
+}
