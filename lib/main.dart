3d01bc5180e7e321786b3b7052fcc7c3d061e0e6
gabrielbaute <gabrielbaute@gmail.com>
2026-07-26 09:42:59 -0400

    Actualización de dependencias: agregamos provider para la gestión de estados

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/dolar_promedio_provider.dart';
+import 'providers/realtime_provider.dart';
+import 'providers/history_provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => DolarPromedioProvider()),
+        ChangeNotifierProvider(create: (_) => RealtimeProvider()),
+        ChangeNotifierProvider(create: (_) => HistoryProvider()),
+      ],
+      child: const MainApp(),
+    ),
+  );
 }
 
+/// Widget raíz de la aplicación.
+///
+/// Attributes:
+///   - key (Key?): Llave identificadora del widget.
 class MainApp extends StatelessWidget {
+  /// Constructor de MainApp.
+  ///
+  /// Args:
+  ///   key (Key?): Llave del widget.
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('DolarVzla App'))),
     );
   }
 }
